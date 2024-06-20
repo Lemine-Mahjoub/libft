a3e18be8402fb9afcea541447c3cdb127f5ed536
grey <leminemahjoub@gmail.com>
2024-06-20 12:31:36 +0200

    ft_isdigit

--- lib/libft.h
+++ lib/libft.h
@@ -6,7 +6,7 @@
 /*   By: grey <grey@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/11 06:50:35 by grey              #+#    #+#             */
-/*   Updated: 2024/06/20 12:19:50 by grey             ###   ########.fr       */
+/*   Updated: 2024/06/20 12:30:32 by grey             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,6 @@
 int	ft_isalpha(int c);
 int	ft_islower(int c);
 int	ft_isupper(int c);
+int	ft_isdigit(int c);
 
 #endif
--- test/t_isdigit.c
+++ test/t_isdigit.c
@@ -1,19 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   test.h                                             :+:      :+:    :+:   */
+/*   t_isdigit.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: grey <grey@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/06/11 07:12:17 by grey              #+#    #+#             */
-/*   Updated: 2024/06/20 12:30:36 by grey             ###   ########.fr       */
+/*   Created: 2024/06/20 12:29:34 by grey              #+#    #+#             */
+/*   Updated: 2024/06/20 12:30:46 by grey             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef TEST_H
-# define TEST_H
+#include "test.h"
+#include <stdio.h>
+#include "../lib/libft.h"
+#include <assert.h>
+#include <ctype.h>
 
-void	t_isalpha(void);
-void	t_isdigit(void);
+void	t_isdigit(void)
+{
+	int	i;
 
-#endif
+	i = 0;
+	while (i < 128)
+	{
+		assert(ft_isdigit(i) == isdigit(i));
+		i++;
+	}
+	printf("ft_isdigit: OK\n");
+}
