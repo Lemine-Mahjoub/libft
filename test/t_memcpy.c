069950855e388324d8bff530ffdbc747ce01c963
grey <leminemahjoub@gmail.com>
2024-06-21 10:12:42 +0200

    fix test t_memcpy.c

--- test/t_memcpy.c
+++ test/t_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: grey <grey@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/21 09:47:23 by grey              #+#    #+#             */
-/*   Updated: 2024/06/21 09:57:59 by grey             ###   ########.fr       */
+/*   Updated: 2024/06/21 10:12:22 by grey             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,17 +21,19 @@ void	t_memcpy(void)
 {
 	char	*str;
 	char	*str2;
+	char	*str3;
+	char	*str4;
 
 	str = malloc(sizeof(char) * 13);
 	strcpy(str, "Hello World");
 	str2 = malloc(sizeof(char) * 13);
 	strcpy(str2, "World Hello");
-	assert(strcmp(ft_memcpy(str, str2, 5), memcpy(str, str2, 5)) == 0);
-	assert(ft_memcpy(str, str2, 2) == memcpy(str, str2, 2));
-	assert(strcmp(ft_memcpy(str, str2, 10), memcpy(str, str2, 10)) == 0);
-	assert(strcmp(ft_memcpy(str, str2, 13), memcpy(str, str2, 13)) == 0);
-	assert(strcmp(ft_memcpy(str, str2, 0), memcpy(str, str2, 0)) == 0);
-	free(str);
-	free(str2);
+	str3 = malloc(sizeof(char) * 13);
+	strcpy(str3, "Hello World");
+	str4 = malloc(sizeof(char) * 13);
+	strcpy(str4, "World Hello");
+	assert(memcmp(ft_memcpy(str, str2, 5), memcpy(str3, str4, 5), 5) == 0);
+	assert(strcmp(str, str3) == 0);
+	assert(strcmp(str2, str4) == 0);
 	printf("ft_memcpy: OK\n");
 }
